94f7a5f4acb2d419998fb9a1a9131c96c96e7b3c
Hiroto Shimabukuro <e175765@ie.u-ryukyu.ac.jp>
2019-11-05 17:11:34 +0900

    add regression_ver2

--- regression.py
+++ regression.py
@@ -5,14 +5,27 @@ class LinearRegression:
     >>> import regression
     >>> model = regression.LinearRegression()
     >>> model.x
-    >>> nothing
+    >>> #nothing
     """
     x = None
     theta = None
     y = None
 
     def fit(self, x, y):
-        pass
+        """
+        >>> import importlib
+        >>> import regression
+        >>> import main
+        >>> X,Y = main.load_linear_example1()
+        >>> importlib.reload(regression)
+        <module 'regression' from '/Users/e175765/ml/regression.py'>
+        >>> model = regression.LinearRegression()
+        >>> model.fit(X,Y)
+        >>> model.theta
+        array([5.30412371, 0.49484536])
+        """
+        temp = np.linalg.inv(np.dot(x.T,x))
+        self.theta = np.dot(np.dot(temp,x.T),y)
 
     def predict(self, x):
         pass
